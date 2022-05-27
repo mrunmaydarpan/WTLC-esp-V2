348adcd479aab8168c13135012823ad51a8961c6
mrunmaydarpan <mrunmay9776@gmail.com>
2022-05-27 13:48:50 +0530

    added comments

--- include/setting.h
+++ include/setting.h
@@ -12,18 +12,18 @@ const char brand[] = "MDtronix";
 #define WM_SET true  // wifimanager
 
 /*------------API setting--------------------*/
-#define HA_INIT false
+#define HA_INIT false // if using with home assistant
 #if HA_INIT
-#define BROKER_ADDR IPAddress(192, 168, 1, 7)
+#define BROKER_ADDR IPAddress(192, 168, 1, 7) home assistant IP
 #endif
 /*-------------------------------------------*/
 
 #define SW_TEST true
-#define OLED true
+#define OLED true // use OLED display
 #define DryRun
 #define debugData false
 #define Buzzer true
-#define relayPin_inverted true
+#define relayPin_inverted false
 // #define web_setting
 
 #define on_delay 500
